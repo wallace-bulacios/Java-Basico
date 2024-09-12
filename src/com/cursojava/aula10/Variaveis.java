0106f9c0abfc505c4c7da3dc8c25ab65903bd792
Wallace V. I. Bulacios <bulacios.dev@gmail.com>
2024-09-12 10:10:53 -0300

    Commit variáveis

--- src/com/cursojava/aula10/Variaveis.java
+++ src/com/cursojava/aula10/Variaveis.java
@@ -3,7 +3,16 @@ package com.cursojava.aula10;
 public class Variaveis {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		int idade = 20;
+		String nome = "Wallace";
+		String nomeDoMeuCachorro = "Carminha";
+		
+		idade = 25;
+		
+		System.out.println("Idade " + idade);
+		System.out.println("Nome " + nome);
+		System.out.println("Nome do meu cachorro é " + nomeDoMeuCachorro);
 
 	}
 
